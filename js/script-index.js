1b3075446fc7e5da236c83dd2c1acc8c59b27e71
Alejandro Israel Gonzalez <imthebest_1997@hotmail.com>
2021-08-10 18:55:27 -0500

    Pagina actualizada

--- js/script-index.js
+++ js/script-index.js
@@ -25,35 +25,5 @@ navToggle.addEventListener("click",function(){
     }
 });
 
-
-window.addEventListener('load', function(){
-    new Glider(document.querySelector(".carousel__lista"), {
-        slidesToShow: 1,
-        dots: '.carousel__indicadores',
-//        draggable: true,
-        arrows: {
-          prev: '.carousel__anterior',
-          next: '.carousel__siguiente'
-        },
-        responsive: [
-            {
-              // screens greater than >= 775px
-              breakpoint: 800,
-              settings: {
-                // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            },{
-              // screens greater than >= 1024px
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-          ]
-        
-    });
-            
-});
+let today = new Date().toISOString().substr(0, 10);
+document.querySelector("#today").value = today;
